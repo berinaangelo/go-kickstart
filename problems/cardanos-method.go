7c0ef3d816474c0b88f05a1df38c1ecbfa32751b
kornbip69 <berinaangelo@gmail.com>
2026-08-19 09:44:07 +0800

    feat: add some problems to work on while learning Go

---
+++ problems/cardanos-method.go
@@ -0,0 +1,3 @@
+// See reference: https://en.wikipedia.org/wiki/Cubic_equation
+
+package problems
---
+++ problems/collatz-conjecture.go
@@ -0,0 +1,8 @@
+// For all 2.36×10^21 > n > 0:
+// write a method that proves the collatz conjecture given n as an input
+// while n do:
+//	if n is even; n = n / 2
+//  else; n = 3n + 1
+// Output: all the possible results of that number, until it n == 1 (this might cause a repeated infinite loop of 1, 2, 4, 2, 1 ...)
+
+package problems
