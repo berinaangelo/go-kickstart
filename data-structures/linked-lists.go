f6625a10a408b3bb7f02b4b430ac8b34a944c9bd
kornbip69 <berinaangelo@gmail.com>
2026-08-19 16:05:01 +0800

    linked list and more pointers

--- data-structures/linked-lists.go
+++ data-structures/linked-lists.go
@@ -1,1 +1,36 @@
 package main
+
+import "fmt"
+
+type Node struct {
+	data int
+	next *Node
+}
+
+type LinkedList struct {
+	head *Node
+	size int
+}
+
+func (ll *LinkedList) Create(data int) {
+	newNode := &Node{data: data, next: nil}
+	if ll.head == nil {
+		ll.head = newNode
+	} else {
+		current := ll.head
+		for current.next != nil {
+			current = current.next
+		}
+		current.next = newNode
+	}
+	ll.size++
+}
+
+func (ll *LinkedList) Display() {
+	current := ll.head
+	for current != nil {
+		fmt.Printf("| %d -> %v | ->", current.data, &current.next)
+		current = current.next
+	}
+	fmt.Println("nil")
+}
--- data-structures/main.go
+++ data-structures/main.go
@@ -1,7 +1,15 @@
 package main
 
 func main() {
-	initArray()
-	initMaps()
-	initStruct()
+	// initArray()
+	// initMaps()
+	// initStruct()
+
+	ll := &LinkedList{}
+	ll.Create(55)
+	ll.Create(68)
+	ll.Create(87)
+	ll.Create(29)
+
+	ll.Display()
 }
