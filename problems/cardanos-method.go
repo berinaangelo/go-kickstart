8949c4c543a6d30b5ccbcd0a508758b3ddc1ecdd
kornbip69 <berinaangelo@gmail.com>
2026-08-19 10:17:16 +0800

    feat: loop approach for collatz conjecture

--- problems/cardanos-method.go
+++ problems/cardanos-method.go
@@ -1,3 +1,5 @@
 // See reference: https://en.wikipedia.org/wiki/Cubic_equation
+// sample input: 3x^3 + 6x^2 + 8x = 0, what the program will do is to extract the a, b and c out of the input equation
+// sample output: the possible roots in order to prove the equation [1.5, 3.6, 5.2]
 
 package main
