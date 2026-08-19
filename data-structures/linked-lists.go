f808ba8f709de2e54200f9420ca9aa106b3d31fc
kornbip69 <berinaangelo@gmail.com>
2026-08-19 16:42:22 +0800

    feat: crud operations in linked list

--- data-structures/linked-lists.go
+++ data-structures/linked-lists.go
@@ -29,8 +29,62 @@ func (ll *LinkedList) Create(data int) {
 func (ll *LinkedList) Display() {
 	current := ll.head
 	for current != nil {
-		fmt.Printf("| %d -> %v | ->", current.data, &current.next)
+		fmt.Printf("[ %d | ● ]->", current.data)
 		current = current.next
 	}
 	fmt.Println("nil")
+}
+
+func (ll *LinkedList) Show(index int) (*Node, error) {
+	if index < 0 || index >= ll.size {
+		return nil, fmt.Errorf("index out of bounds")
+	}
+
+	current := ll.head
+	for i := 0; i < index; i++ {
+		current = current.next
+	}
+
+	return current, nil
+}
+
+func (ll *LinkedList) Search(data int) (int, error) {
+	current := ll.head
+	index := 0
+
+	for current != nil {
+		if current.data == data {
+			return index, nil
+		}
+		current = current.next
+		index++
+	}
+
+	return -1, fmt.Errorf("data not found")
+}
+
+func (ll *LinkedList) Update(index int, newData int) (*Node, error) {
+	current, _ := ll.Show(index)
+	current.data = newData
+
+	return current, nil
+}
+
+func (ll *LinkedList) Delete(index int) error {
+	if index < 0 || index >= ll.size {
+		return fmt.Errorf("index out of bounds")
+	}
+
+	if index == 0 {
+		ll.head = ll.head.next
+	} else {
+		current := ll.head
+		for i := 0; i < index-1; i++ {
+			current = current.next
+		}
+		current.next = current.next.next
+	}
+	ll.size--
+
+	return nil
 }
--- data-structures/main.go
+++ data-structures/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	// initArray()
 	// initMaps()
@@ -11,5 +13,16 @@ func main() {
 	ll.Create(87)
 	ll.Create(29)
 
+	ll.Display()
+
+	data, _ := ll.Show(1)
+	fmt.Println(data.data) // index accepted as param
+
+	fmt.Println(ll.Search(29)) // data accepted as param
+
+	ll.Update(0, 65)
+	ll.Display()
+
+	ll.Delete(0)
 	ll.Display()
 }
