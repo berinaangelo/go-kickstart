1d29fd36ac29530826032e182bc1978fa65fecd0
kornbip69 <berinaangelo@gmail.com>
2026-08-19 16:59:44 +0800

    wip: stack data structures (nodes format)

--- data-structures/stack.go
+++ data-structures/stack.go
@@ -1,1 +1,34 @@
 package main
+
+import "fmt"
+
+type Stack struct {
+	top  *Node
+	size int
+}
+
+func (s *Stack) Push(data int) {
+	s.top = &Node{data: data, next: s.top}
+	s.size++
+}
+
+func (s *Stack) Pop() (int, error) {
+	if s.top == nil {
+		return 0, fmt.Errorf("stack is empty")
+	}
+
+	data := s.top.data
+	s.top = s.top.next
+	s.size--
+	return data, nil
+}
+
+func (s *Stack) Display() {
+	current := s.top
+
+	for current != nil {
+		fmt.Printf("[ %d ]", current.data)
+		current = current.next
+	}
+	fmt.Println("nil")
+}
--- problems/linked-list-crud.go
+++ problems/linked-list-crud.go
@@ -1,1 +1,2 @@
+// TODO: make a simple management console-based system of a linked list that can perform crud operations
 package main
